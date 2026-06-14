4cd21ddbde15f70fe2f4f13ae02baaf3319dcad0
vanchiu5288 <vanchiu@LAPTOP-CNOAALII.localdomain>
2026-06-14 23:39:17 +0800

    asadsda

--- backend/auth_api/urls.py
+++ backend/auth_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, DiscordWebhookView
 
 urlpatterns = [
-    # 對應到 /api/login (實際完整網址取決於專案根目錄的 urls.py 如何 include)
-    path('login/', LoginView.as_view(), name='ldap_login'),
+    path("login/", LoginView.as_view(), name="ldap_login"),
+    path("discord-webhook/", DiscordWebhookView.as_view(), name="discord_webhook"),
 ]
